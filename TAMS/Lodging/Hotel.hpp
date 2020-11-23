2e39f6cf9001d07d47f6ab301f529d99a7003e9e
Raghav Gupta <2001guptar@gmail.com>
2020-11-23 10:19:08 -0800

    added more files

---
+++ TAMS/Lodging/Hotel.hpp
@@ -0,0 +1,19 @@
+#ifndef _HOTEL_HPP_
+#define _HOTEL_HPP_
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Hotel : public Hotel {
+    private:
+      
+    public:
+
+};  
+
+
+#endif
