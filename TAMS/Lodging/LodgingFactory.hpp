34e685454bcdd2e2050f28b81fd6b20a15c8beb5
Raghav Gupta <2001guptar@gmail.com>
2020-12-08 21:16:44 -0800

    added file for lodgingfactory containing generate function

---
+++ TAMS/Lodging/LodgingFactory.hpp
@@ -0,0 +1,27 @@
+#ifndef _LODGINGFACTORY_HPP_
+#define _LODGINGFACTORY_HPP_
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+#include "Lodging.hpp"
+#include "Motel.hpp"
+#include "Hotel.hpp"
+#include "AirBnB.hpp"
+#include "Resort.hpp"
+
+using namespace std;
+
+class LogdingFactory 
+{
+public:
+    LogdingFactory() {}
+    Lodging *generate()
+    {
+    
+    }
+};
+
+#endif
