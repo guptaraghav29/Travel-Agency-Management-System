60cf746ebff470ee278c604021df69a0e378b644
pdarb <pdarb001@ucr.edu>
2020-12-08 21:12:37 -0800

    Changed factory names

--- TAMS/Transportation/Cruise.hpp
+++ TAMS/Transportation/Cruise.hpp
@@ -7,7 +7,7 @@
 #include "ConcreteRoute.hpp"
 #include "ConcreteTicket.hpp"
 
-class CruiseFirst : public TransportationFactory {
+class CruiseSuite : public TransportationFactory {
     public:
         Route* getRoute() {
             return new CruiseRoute();
