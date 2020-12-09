69d31cc12c79fa5d2359dd16de8091946393e458
Lawrence Wang <lawrence.g.wang@gmail.com>
2020-12-09 14:45:09 -0800

    Add destructors to PointOfInterest

--- TAMS/PointOfInterest/PointOfInterest.hpp
+++ TAMS/PointOfInterest/PointOfInterest.hpp
@@ -16,7 +16,8 @@ class PointOfInterest {
         double price;
     public:
         PointOfInterest() { }
-        PointOfInterest(std::string n, std::string t, double p) : name(n), type(t), price(p) { };
+        PointOfInterest(std::string n, std::string t, double p) : name(n), type(t), price(p) { }
+        virtual ~PointOfInterest() { }
         virtual std::string getInfo() = 0;
         virtual double getPrice() = 0;
         void print();
@@ -24,7 +25,6 @@ class PointOfInterest {
 };
 
 class GenericPOI : public PointOfInterest {
-    private:
     public:
         GenericPOI(std::string n, std::string t, double p) : PointOfInterest(n, t, p) { };
         virtual std::string getInfo() override;
@@ -36,7 +36,7 @@ class InfoDecorator : public PointOfInterest {
         PointOfInterest* poi;
     public:
         InfoDecorator(PointOfInterest* point) : poi(point) { };
-        ~InfoDecorator() { delete poi; }
+        virtual ~InfoDecorator() { delete poi; }
         virtual std::string getInfo() = 0;
         virtual double getPrice() override;
 };
@@ -46,7 +46,7 @@ class PhoneDecorator : public InfoDecorator {
         std::string number;
     public:
         PhoneDecorator(PointOfInterest* point, std::string n) : InfoDecorator(point), number(n) { };
-        std::string getInfo();
+        virtual std::string getInfo() override;
 };
 
 class DescriptionDecorator : public InfoDecorator {
@@ -54,7 +54,7 @@ class DescriptionDecorator : public InfoDecorator {
         std::string description;
     public:
         DescriptionDecorator(PointOfInterest* point, std::string d) : InfoDecorator(point), description(d) { };
-        std::string getInfo();
+        virtual std::string getInfo() override;
 };
 
 class RatingDecorator : public InfoDecorator {
@@ -62,7 +62,7 @@ class RatingDecorator : public InfoDecorator {
         double rating;
     public:
         RatingDecorator(PointOfInterest* point, double r) : InfoDecorator(point), rating(r) { };
-        std::string getInfo();
+        virtual std::string getInfo() override;
 };
 
 class AddressDecorator : public InfoDecorator {
@@ -70,7 +70,7 @@ class AddressDecorator : public InfoDecorator {
         std::string address;
     public:
         AddressDecorator(PointOfInterest* point, std::string a) : InfoDecorator(point), address(a) { };
-        std::string getInfo();
+        virtual std::string getInfo() override;
 };
 
 class TimeDecorator : public InfoDecorator {
@@ -78,7 +78,7 @@ class TimeDecorator : public InfoDecorator {
         std::string time;
     public:
         TimeDecorator(PointOfInterest* point, std::string t) : InfoDecorator(point), time(t) { };
-        std::string getInfo();
+        virtual std::string getInfo() override;
 };
 
 
