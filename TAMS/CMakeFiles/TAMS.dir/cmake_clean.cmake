8b40f71050098bfcaf967059d7149c221ba2221a
Raghav Gupta <2001guptar@gmail.com>
2020-12-09 22:56:30 -0800

    Added screenshots

---
+++ TAMS/CMakeFiles/TAMS.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TAMS.dir/PointOfInterest/POIFactory.cpp.o"
+  "CMakeFiles/TAMS.dir/PointOfInterest/PointOfInterest.cpp.o"
+  "CMakeFiles/TAMS.dir/Price/PriceVisitor.cpp.o"
+  "CMakeFiles/TAMS.dir/client.cpp.o"
+  "CMakeFiles/TAMS.dir/main.cpp.o"
+  "CMakeFiles/TAMS.dir/trip.cpp.o"
+  "TAMS"
+  "TAMS.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TAMS.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
